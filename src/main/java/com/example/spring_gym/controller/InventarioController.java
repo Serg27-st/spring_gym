80614167684ff068ca86396a5aa3086b90abcbd5
Sergio Ricardo Tafur Betetta <stafur@gca.local>
2025-10-27 12:59:21 -0500

    prueba de imagenes

--- src/main/java/com/example/spring_gym/controller/InventarioController.java
+++ src/main/java/com/example/spring_gym/controller/InventarioController.java
@@ -1,5 +1,6 @@
 package com.example.spring_gym.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.spring_gym.model.Inventario;
 import com.example.spring_gym.model.Socio;
 import com.example.spring_gym.services.InventarioService;
+import com.example.spring_gym.services.UploadFileService;
 
 
 
@@ -26,6 +30,9 @@ public class InventarioController {
      private final Logger LOGGER = LoggerFactory.getLogger(InventarioController.class);
      @Autowired
      private InventarioService inventarioService; 
+
+     @Autowired
+     private UploadFileService upload;
 
      
     @GetMapping("")
@@ -39,12 +46,18 @@ public class InventarioController {
     }
 
     @PostMapping("/save")
-    public String save(Inventario inventario){
+    public String save(Inventario inventario, @RequestParam("img") MultipartFile file) throws IOException{
      LOGGER.info("ESTE ES EL OBJETO INVENTARIO{}", inventario);
      Socio s = new Socio(1, "", "", "", "", "", "", "");
      inventario.setSocio(s);
+     //imagen
+     if (inventario.getIdInventario()==null){
+          String nombreImagen = upload.saveImage(file);
+          inventario.setImagen(nombreImagen);
+     }else {
+         
+     }
      inventarioService.save(inventario);
-     
      return "redirect:/inventarios";
     }
 
@@ -59,14 +72,39 @@ public class InventarioController {
      return "inventarios/edit";
     }
     @PostMapping("/update")
-     public String update(Inventario inventario) {
+     public String update(Inventario inventario, @RequestParam("img") MultipartFile file) throws IOException {
+          Inventario i=new Inventario();
+          i=inventarioService.get(inventario.getIdInventario()).get();
+          
+          if(file.isEmpty()) {
+             
+               inventario.setImagen(i.getImagen());
+          } else {// cuando se edita tbn la imagen			
+			//eliminar cuando no sea la imagen por defecto
+
+          if (!i.getImagen().equals("default.jpg") ) {
+              upload.deleteImage(i.getImagen());
+          }
+
+               String nombreImagen = upload.saveImage(file);
+          inventario.setImagen(nombreImagen);
+          }
+          
           inventarioService.update(inventario);
      return "redirect:/inventarios";
     }
 
+
+
      @GetMapping("/delete/{id}")
      public String delete(@PathVariable Integer id) {
-          inventarioService.delete(id);;
+          Inventario i=new Inventario();
+          i=inventarioService.get(id).get();
+          
+          if (!i.getImagen().equals("default.jpg") ) {
+              upload.deleteImage(i.getImagen());
+          }
+          inventarioService.delete(id);
      return "redirect:/inventarios";
     }
     
--- src/main/java/com/example/spring_gym/services/SocioService.java
+++ src/main/java/com/example/spring_gym/services/SocioService.java
@@ -1,6 +1,7 @@
 package com.example.spring_gym.services;
 
 import java.util.Optional;
+
 import com.example.spring_gym.model.Socio;
 
 public interface SocioService {

482db3bda431a01fa1727a6ebfa611f768f76309
Sergio Ricardo Tafur Betetta <stafur@gca.local>
2025-11-18 09:17:26 -0500

    obtener la cantidad y buscar el producto

--- src/main/java/com/example/spring_gym/controller/HomeController.java
+++ src/main/java/com/example/spring_gym/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.example.spring_gym.controller;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,10 +13,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.spring_gym.model.DetalleVenta;
 import com.example.spring_gym.model.Inventario;
+import com.example.spring_gym.model.Venta;
 import com.example.spring_gym.services.InventarioService;
+
 
 
 @Controller
@@ -26,6 +33,12 @@ public class HomeController {
 	@Autowired
 	private InventarioService inventarioService;
 
+	List<DetalleVenta> detalles = new ArrayList<DetalleVenta>();
+
+	Venta venta = new Venta();
+
+
+
 	@GetMapping("")
 	public String home(Model model) {
 				
@@ -35,13 +48,26 @@ public class HomeController {
 	}
 
 
-	@GetMapping("productohome/{id}")
+	@GetMapping("inventariohome/{id}")
 	public String inventarioHome(@PathVariable Integer id, Model model){
 		log.info("id producto enviado como parametro[]",id);
 		Inventario inventario = new Inventario();
 		Optional<Inventario> inventarioOptional= inventarioService.get(id);
 		inventario = inventarioOptional.get();
 		model.addAttribute("inventario",inventario);
-		return "socio/productohome";
+		return "socio/inventariohome";
 	}
+
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+		DetalleVenta detalleVenta = new DetalleVenta();
+		Inventario inventario = new Inventario();
+		double sumatotal = 0;
+		Optional<Inventario> optionaInventario = inventarioService.get(id);
+		log.info("Producto añadido: {}", optionaInventario.get());
+		log.info("cantidad {}", cantidad);
+		
+		return "socio/carrito";
+	}
+	
 }
